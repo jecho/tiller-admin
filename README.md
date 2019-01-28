b8e95cee29373e359bd034bb34582c6d86e6c8a6
Jecho <jecho@users.noreply.github.com>
2019-01-28 15:02:54 -0800

    Update README.md
    initial commit
    
    Updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# tiller-clusterole
+# Usage
+
+## OSX
+```
+curl -o setup.sh https://raw.githubusercontent.com/jecho/tiller-admin/master/cluster-admin.sh
+chmod +x setup.sh
+./setup.sh
+```
---
+++ cluster-admin.sh
@@ -0,0 +1,3 @@
+kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'      
+helm init --service-account tiller --upgrade
